fc13aeb6f31cd72cb1f2c99e6d34441aae2ee334
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-07-19 20:29:03 +0800

    add mysql

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"genki/models"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+var DB *gorm.DB
+
+func connectDatabase() {
+	database, err := gorm.Open(mysql.Open("fishmans:fishmans@tcp(127.0.0.1:3306)/note?charset=utf8"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	DB = database
+}
+
+func dbMigrate() {
+	DB.AutoMigrate(&models.Note{})
+}
 
 func main() {
 	r := gin.Default()
@@ -14,6 +32,9 @@ func main() {
 	r.Static("vendor", "./static/vendor")
 
 	r.LoadHTMLGlob("templates/**/**")
+
+	connectDatabase()
+	dbMigrate()
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "views/index.html", gin.H{
